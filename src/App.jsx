b3f37538feca61eeac036612230ba1836b651849
Petar Mitrevichin <pmitrevichin@gmail.com>
2024-09-01 14:42:59 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './Header';
+import Major from './Major';
+
+export default function App() {
+  return (
+    <div className='app'>
+      <Header />
+
+      <Major>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Major>
+    </div>
+  );
+}
